d980342ba0f4595230dcb3c23234daf6a73ddf8e
Austin Longo <austinlongocontracting@gmail.com>
2021-12-23 16:33:01 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+
+  function handleToDo(toDo){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = ' x '
+    p.textContent = `${toDo} `
+    p.appendChild(btn)
+    document.querySelector("#tasks").appendChild(p)
+    console.log(p)
+  }
+
+
 });
+//
+//document.querySelector('form').addEventListener('', )
+
+
+// handle delete button
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
+
+//grab x buttons
+function addEventListenerToDeleteButtons() {
+  let button = document.getElementsByClassName('delete_btn')
+  for (let btn of buttons) {
+      btn.addEventListener('click', )
+  }
+}
